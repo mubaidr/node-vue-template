b0b230b25ae44b02550fded66adfcfe1d57eb6c2
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-11-09 08:08:32 -0800

    remove unused package

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var path = require('path')
-var favicon = require('serve-favicon')
+//var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
@@ -26,14 +26,14 @@ app.use('/', index)
 app.use('/users', users)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -22,7 +22,7 @@
 <script>
   export default {
     name: 'app',
-    data() {
+    data () {
       return {
         msg: 'Welcome to Your Vue.js App'
       }
