6efff6a8674f5505000050d763734a420017371f
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-11-10 10:38:23 +0500

    simplify routes, add dev script

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,11 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
-var index = require('./routes/index')
-var users = require('./routes/users')
-
+var routes = require('./routes/index')
 var app = express()
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
 app.use(bodyParser.json())
@@ -22,8 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index)
-app.use('/users', users)
+app.use('/', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
-})
+// route segments
+var login = require('./login')
+
+router.use('/login', login)
 
 module.exports = router
