e376463f472beb2cee732762d0f32363ca784aad
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-11-12 17:53:36 +0500

    simplify utilities import

--- frontend/src/utilities/index.js
+++ frontend/src/utilities/index.js
@@ -1,16 +1,11 @@
-import 'axios'
-import 'moment'
-import 'vuejs-datepicker'
-
+import './pollyfills'
 import './axios.js'
 import './moment.js'
-import './datepicker'
+import './vuejs-datepicker'
+import './vee-validate'
 
 import store from '../store'
 import router from '../router'
 import { sync } from 'vuex-router-sync'
 
-import promise from 'es6-promise'
-
 sync(store, router)
-promise.polyfill()
---
+++ frontend/src/utilities/vee-validate/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import veeValidate from 'vee-validate'
+
+Vue.use(veeValidate)
